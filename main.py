70d723aee244aa7d479c0431546088785460b926
Zoya Iftekhar <99093347+zoya-iftekhar@users.noreply.github.com>
2024-12-20 16:50:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# set title
+st.title("Brain MRI Tumor Detection")
+
+# set header 
+st.header('Please upload an image')
+
+# upload image
+st.file_uploader('', type=['png', 'jpg', 'jpeg'])
+# load model 
+
+# load image
+
+# detect objects 
+
+# visualize the objects
+
+
